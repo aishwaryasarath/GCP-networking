40e7588c7d547dc7da0cfdf8d45bf979bc09401f
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2025-05-09 19:16:22 -0500

    Create privatenet.tf

---
+++ tfnet/privatenet.tf
@@ -0,0 +1,21 @@
+# Create privatenet network
+resource "google_compute_network" "privatenet" {
+  name                    = "privatenet"
+  auto_create_subnetworks = false
+}
+
+# Create privatesubnet-us subnetwork
+resource "google_compute_subnetwork" "privatesubnet-us" {
+  name          = "privatesubnet-us"
+  region        = "europe-west1"
+  network       = google_compute_network.privatenet.self_link
+  ip_cidr_range = "172.16.0.0/24"
+}
+
+# Create privatesubnet-second-subnet subnetwork
+resource "google_compute_subnetwork" "privatesubnet-second-subnet" {
+  name          = "privatesubnet-second-subnet"
+  region        = "us-east4"
+  network       = google_compute_network.privatenet.self_link
+  ip_cidr_range = "172.20.0.0/24"
+}
